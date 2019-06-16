54251f661978a28140f5f70d03f32f8696618dff
Dwi Nurhadi Afriyanto <dwi.nurhadi17@student.uns.ac.id>
2019-06-16 08:54:09 +0700

    function di es6

--- src/index.js
+++ src/index.js
@@ -5,9 +5,20 @@
 // var nama = 'dwinurhadi';
 // var nama = 'indonesia';
 
-let nama = 'dwi';
-{
-let nama = 'dnurhadas';
-console.log(nama);
+// let nama = 'dwi';
+// {
+// let nama = 'dnurhadas';
+// console.log(nama);
+// }
+// console.log(nama);
+
+function getName() {
+    if (true) {
+        var nama = "adadsa";
+        console.log(nama);
+    } else {
+        console.log(nama);
+    }
 }
-console.log(nama);
+
+getName();
