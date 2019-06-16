79792e60823f370679c592d4f5abe579f478590a
Dwi Nurhadi Afriyanto <dwi.nurhadi17@student.uns.ac.id>
2019-06-16 08:49:06 +0700

    beda let dan var

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
-class Test {
+//var nama = 'Dwi Nurhadi'; //merupakan function scope
 
+// let dan const merupakan blockscope
+
+// var nama = 'dwinurhadi';
+// var nama = 'indonesia';
+
+let nama = 'dwi';
+{
+let nama = 'dnurhadas';
+console.log(nama);
 }
-
-console.log(Test);
+console.log(nama);
